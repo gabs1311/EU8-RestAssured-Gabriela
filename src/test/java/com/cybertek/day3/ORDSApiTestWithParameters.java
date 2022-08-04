f249845775cc6e845d6bcd13ada330b08061b201
gabs1311 <gabs.antonovici@gmail.com>
2022-08-04 21:13:13 +0300

    day 3- ords api test with parameters

--- src/test/java/com/cybertek/day3/ORDSApiTestWithParameters.java
+++ src/test/java/com/cybertek/day3/ORDSApiTestWithParameters.java
@@ -2,7 +2,11 @@ package com.cybertek.day3;
 
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,7 +30,41 @@ public class ORDSApiTestWithParameters {
      */
 
 
+    @DisplayName("GET request to /countries with Query Param")
+    @Test
+    public void test1(){
+        Response response= given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"region_id\":2}")
+                .log().all()
+                .when()
+                .get("/countries");
 
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.header("Content-Type"));
+        assertTrue(response.body().asString().contains("United States of America"));
+
+        response.prettyPrint();
+
+    }
+    /*
+        Send a GET request to employees and get only employees who works as a IT_PROG
+
+     */
+
+    @DisplayName("GET request to /employees with Query Param")
+    @Test
+    public void test2(){
+        Response response= given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"job_id\": \"IT_PROG\"}")
+                .when().get("/employees");
+
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.header("Content-Type"));
+        assertTrue(response.body().asString().contains("IT_PROG"));
+
+        response.prettyPrint();
+
+    }
 
 
 }
