9ebcb60c8c329463714830d290edad6a692fb027
gabs1311 <gabs.antonovici@gmail.com>
2022-09-29 00:58:13 +0300

    ORDS Api with Json path

---
+++ src/test/java/com/cybertek/day4/ORDSApiWithJsonPath.java
@@ -0,0 +1,21 @@
+package com.cybertek.day4;
+
+import com.cybertek.utility.HrTestBase;
+
+import com.cybertek.utility.HrTestBase;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ORDSApiWithJsonPath extends HrTestBase {
+
+
+}
