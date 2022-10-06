80c7f0cdfb164cb17f466b1354cdb72f4eb8c95a
gabs1311 <gabs.antonovici@gmail.com>
2022-10-06 22:56:50 +0300

    ORDS Api with Json path  - added more

--- src/test/java/com/cybertek/day4/ORDSApiWithJsonPath.java
+++ src/test/java/com/cybertek/day4/ORDSApiWithJsonPath.java
@@ -5,6 +5,7 @@ import com.cybertek.utility.HrTestBase;
 import com.cybertek.utility.HrTestBase;
 
 import io.restassured.http.ContentType;
+import io.restassured.internal.RestAssuredResponseOptionsGroovyImpl;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -37,8 +38,44 @@ public class ORDSApiWithJsonPath extends HrTestBase {
     List<String> allCountrieIds = jsonPath.getList("items.country_id");
     System.out.println("allCountrieIds = " + allCountrieIds);
 
+    // get all country names where their region id is equal to 2
 
+    List<String> countryNamesWithRegionId2 = jsonPath.getList("items.findAll {it.region_id==2}.country_name");
+    System.out.println("countryNamesWithRegionId2 = " + countryNamesWithRegionId2);
 }
+
+@DisplayName("GET request to / employees with query param ")
+@Test
+    public void test2(){
+    // we added query param to get 107 employees
+    given().queryParams("limit", 107)
+            .when().get("/employees");
+
+    // first if statement:
+    // get me all email of employees who is working as IT_PROG=(if IT_PROG is the job, I want to get the email)
+    //we added limit query param to get 107 employees
+    Response response = given().queryParam("limit", 107)
+            .when().get("/employees");
+
+    JsonPath jsonPath = response.jsonPath();
+    //response.prettyPrint();
+    //get me all email of employees who is working as IT_PROG
+    List<String> employeeITProgs = jsonPath.getList("items.findAll {it.job_id==\"IT_PROG\"}.email");
+    System.out.println(employeeITProgs);
+
+    // get me first name of employees who is making more than 10000
+
+        List<String> empNames = jsonPath.getList("items.findAll{it.salary>10000}.first_name");
+    System.out.println("empNames = " + empNames);
+
+
+    // gte the max salary first_name
+    String kingFirstName = jsonPath.getString("items.max{it.salary}.first_name");
+    Object kingNameWithPathMethod = response.path("items.max{it.salary}.first_name");// it works juts as well
+    // as the previous line
+    System.out.println("kingFirstName = " + kingFirstName);
+    System.out.println("kingNameWithPathMethod = " + kingNameWithPathMethod);
+        }
 
 
 
